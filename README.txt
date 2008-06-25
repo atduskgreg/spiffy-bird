fc3abd8f8e5f73ae98efdd82c23d27623f7114ea
Greg Borenstein <greg@grabb.it>
2008-06-25 16:03:45 -0700

    * brought the code over from the secret songbirdapp * added MIT and GPL3 licenses * broke out documentation into its own file

---
+++ README.txt
@@ -0,0 +1,81 @@
+ Spiffy Bird: JSPF to Songbird Bridge
+ Version: 0.6 6/25/2008 
+ by Greg Borenstein
+
+///////////////////////////////////////////////////////////////////////////////
+ 
+ Spiffy Bird provides a bridge between the Songbird music player 
+ webpage API and the JSON Shareable Playlist Format. It allows javascript 
+ applications that work with JSPF to easily interoperate with Songbird.
+ Further, with the help of jchris's xspf_parser:
+
+ http://jchris.mfdz.com/static/jspf_parser/xspf_parser_runner.html
+
+ Spiffy Bird makes it a snap to go from XSPF playlists available at public 
+ URLs all the way into local Songbird playlists (check out the example 
+ at the bottom of this document).
+
+ JSPF is the javascript version of XSPF, the XML Sharable Playlist Format.
+ See the JSPF draft spec here: http://wiki.xiph.org/index.php/JSPF_Draft
+ and learn more about XSPF here: http://www.xspf.org/
+
+ For an introduction to the Songbird Javascript API read this blog post:
+ http://www.urbanhonking.com/ideasfordozens/2008/06/songbird-media-player-integrat.html
+ 
+ Read the full Songbird JS API docs here: 
+ http://developer.songbirdnest.com/webpage-api/docs/files/sbIRemotePlayer-idl.html
+
+///////////////////////////////////////////////////////////////////////////////
+
+ Spiffy Bird Public API Documentation:
+
+ SpiffyBird.webPlaylist.show(jspf)
+  displays the given playlist in the Web Playlist at the bottom of the main songbird window
+
+ SpiffyBird.webPlaylist.get()
+	return a JSPF representation of the current state of the Web Playlist
+
+ SpiffyBird.localPlaylist.save(jspf)
+	saves the given JSPF into	a local playlist within songbird
+
+ SpiffyBird.localPlaylist.get(query)
+  returns JSPF representations of all playlists that match the given query;
+	queries consist of a hash with a playlist attribute and its expected value, e.g.:
+	SpiffyBird.localPlaylist.get({name : "My Playlist"})
+
+ SongbirdLibrary.allPlaylists()
+	returns an array containing a mediaList representing each local playlist within Songbird
+
+ SongbirdLibrary.find(query)
+	returns an array of mediaLists representing each local playlist within 
+	Songbird that matches the given query; queries consist of a hash 
+	with a playlist attribute and its expected value, e.g.:
+	SpiffyBird.localPlaylist.get({name : "My Playlist"})
+
+ SpiffyBird.utils.JSPFfrom(mediaList)
+ 	returns a JSPF representation of the mediaList
+
+ SpiffyBird.utils.JSPFinto(jspf, mediaList, library)
+ 	populates the mediaList from the given JSPF.
+ 	will use existing mediaItems on the given library if available,
+	otherwise it will add them.
+
+ XSPF usage example:
+
+ get an XSPF document and render it into a local playlist using SpiffyBird, 
+ jchris's , and jquery's $.ajax:
+
+ $.ajax({
+ 	url: "playlist.xspf",
+ 	success: function(xspf_doc){
+ 		var xmlString = XSPF.XMLfromString(xspf_doc);
+ 		var jspf = XSPF.toJSPF(xmlString);
+ 		SpiffyBird.localPlaylist.save(jspf);
+ 		$("h1").html("Saved playlist: "+jspf["playlist"]["title"]);
+ 	}
+ });
+
+ KNOWN ISSUES:
+ - should serialize the rest of the playlist-level metadata into the mediaList properties
+ - right now we fallback to grabb.it mp3 urls when creating mediaItems since we need something...
+ - we fail to use siteLibrary scope options when allowed
---
+++ spiffy_bird.js
@@ -0,0 +1,204 @@
+
+
+// turn off console.log...comment this out in debug mode:
+if (!console || !console.log){
+	var console = {
+		log : function() {return false} 
+	}
+}
+
+var SpiffyBird = (function() {
+
+	/* helper methods */
+
+	function isPublicUrl(url){
+		return (url && /^http/.test(url) && url) || null;
+	};
+
+	function JSPFtracksfromMediaList(list){
+
+		var tracks = []
+
+		list.enumerateAllItems({
+			onEnumerationBegin: function(list) {},
+			onEnumeratedItem: function(list, item) {
+				var track = {}
+				var apiKeys = {
+					title:"http://songbirdnest.com/data/1.0#trackName",
+					creator:"http://songbirdnest.com/data/1.0#artistName",
+					album:"http://songbirdnest.com/data/1.0#albumName",
+					trackNum:"http://songbirdnest.com/data/1.0#trackNumber",
+					duration:"http://songbirdnest.com/data/1.0#duration",
+					image:"http://songbirdnest.com/data/1.0#albumArtURL"
+				}
+
+				for (key in apiKeys) {
+					var val = apiKeys[key];
+					try {
+						track[key] = item.getProperty(val);						
+					} catch(e) {
+						console.log("error with "+val);
+					}
+				}
+				
+				track.location = [];				
+				var ourl = item.getProperty('http://songbirdnest.com/data/1.0#originURL')
+				if (ourl && ourl != "__BLOCKED__"){
+					track.location.push(item.getProperty('http://songbirdnest.com/data/1.0#originURL'));
+				};
+
+				track.identifier = item.guid;
+
+				tracks.push(track);
+			},
+			onEnumerationEnd: function(list) {}
+		});
+		return tracks;	
+	};
+
+
+	function buildJSPFfromMediaList(mediaList){
+		var jspf = {
+			"playlist" : {
+				"title" : mediaList.name,
+				"track" : JSPFtracksfromMediaList(mediaList)
+			}
+		}	
+		return jspf;
+	};
+	
+	function populateMediaListFromJSPF(jspf, mediaList, library){
+		mediaList.clear();
+		mediaList.name = jspf.playlist.title;
+
+		/* use mediaList.setProperty() to serialize playlist level metadata
+		this will not be available to the user
+		*/
+
+		// addtional JSPF properties with no obvious equivalent:
+    // 
+		// annotation
+		// info
+		// link
+		// meta
+		// extention
+    // 
+		// maybe we will syndicate them into namespaced properties?
+		
+		for (var i = 0, l = jspf.playlist.track.length; i < l; i++) {
+			var track = jspf.playlist.track[i];
+			
+			try {
+				
+				if (track.identifier){
+					console.log("tid: "+track.identifier);
+					var mediaItem = library.getItemByGuid(track.identifier);
+				}
+				if (!mediaItem){ // didn't find it in the library or didn't have an identifier to search for
+					console.log("making new media item");
+					// fall back to fake grabb.it mp3 with lack of actual mp3:
+					var mediaItem = library.createMediaItem(track.location[0] || 'http://grabb.it/music/'+track.creator+'/'+track.title+'.mp3');
+				}
+				
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#trackName", 	 ( track.title || null )	);
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#artistName",  ( track.creator || null ) );
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#albumArtURL", ( track.image || null )	);
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#albumName",   ( track.album || null	) );
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#trackNumber", ( track.trackNum || null ) );
+				mediaItem.setProperty( "http://songbirdnest.com/data/1.0#duration", 	 ( track.duration || 0 ) );	
+
+				mediaList.add(mediaItem);
+
+			} catch(e) {
+				console.log("Failed creation of mediaitem with url:"+track.location[0]+" " + e);
+			}
+		}
+		
+		return mediaList
+	}
+	
+	function buildMediaListFromJSPF(jspf, library){
+		var mediaList = library.createSimpleMediaList(jspf.playlist["title"]);
+		mediaList.clear(); // syncing
+
+		return populateMediaListFromJSPF(jspf, mediaList, library);
+	};
+
+	return {
+		
+		/* public methods */
+		
+		utils : {
+			JSPFfrom : function(mediaList){
+				return buildJSPFfromMediaList(mediaList);
+			},
+			JSPFinto : function(jspf, mediaList, library){
+				return populateMediaListFromJSPF(jspf, mediaList, library); 
+			}
+		},
+		
+		webPlaylist : {	
+			show : function(jspf){
+				var library = songbird.siteLibrary; // ignoring scope for now because of conflict with mainLibrary access
+				songbird.webPlaylist.mediaList = buildMediaListFromJSPF(jspf, library);
+			},
+			get : function(){
+				var library = songbird.siteLibrary
+				return buildJSPFfromMediaList(songbird.webPlaylist.mediaList);
+			}
+		},
+		localPlaylist : {	
+			save : function(jspf) {
+				var library = songbird.mainLibrary;
+				buildMediaListFromJSPF(jspf, library); // since we use the main library this actually installs the playlist
+			},
+			get : function(query) {
+				var results = []
+				
+				var mediaListResults = SongbirdLibrary.find(query);
+				for (var i = 0, l = mediaListResults.length; i < l; i++) {
+					results.push(buildJSPFfromMediaList(mediaListResults[i]));
+				}
+				
+				return results;
+			}
+		}
+	}
+})();
+
+var SongbirdLibrary = (function(){
+	return {
+		allPlaylists : function(){
+			var mediaLists = songbird.mainLibrary.getPlaylists();
+			var results = [];
+			while(mediaLists.hasMoreElements()) {
+				var mediaList = mediaLists.getNext();
+				results.push(mediaList);
+			}
+			return results;
+		},
+
+		find : function(query){
+			var library = songbird.mainLibrary;
+			var mediaLists = library.getPlaylists();
+
+			var results = [];
+
+			while(mediaLists.hasMoreElements()) {
+				var this_list_matches = true;
+				var mediaList = mediaLists.getNext();
+
+				for (key in query) {
+					var match = query[key];
+					if (mediaList[key] != match) {
+						this_list_matches = false;
+					}
+				}
+				if (this_list_matches) {
+					results.push(mediaList);
+				}
+			}
+			return results;
+		}
+	};
+})();	
